360ea92f7c24a3c926bcc4a2b67c331fb0aa4ee4
Jan Brazda <brazdahonza@gmail.com>
2023-08-18 15:33:42 +0200

    finished section 2/5, code cleanup

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,36 +1,85 @@
-fun main(args: Array<String>) {
+package indigo
+import java.lang.NumberFormatException
+
+fun main() {
     startGame()
 }
 
 fun startGame() {
+    var cards: MutableList<String> = createDeck()
+    var gameRunning = true
+
+    while(gameRunning) {
+        println("Choose an action (reset, shuffle, get, exit):")
+        val option = readln()
+        when(option) {
+            "reset" -> cards = resetDeck()
+            "shuffle" -> shuffleDeck(cards)
+            "get" -> get(cards)
+            "exit" -> {
+                gameRunning = false
+                println("Bye")
+            }
+            else -> println("Wrong action.")
+        }
+    }
+}
+
+fun createDeck(): MutableList<String> {
     val ranks = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
     val suits = listOf("♦", "♥", "♠", "♣")
     val cards = mutableListOf<String>()
-    var gameRunning: Boolean = true
 
-   while(gameRunning) {
-       println("Choose an action (reset, shuffle, get, exit):")
-       val option = readln()
-       when(option) {
-           "reset" -> resetDeck()
-           "shuffle" -> shuffleDeck()
-           "get" -> get()
-           "exit" -> {
-               gameRunning = false
-               println("Bye")
-           }
-       }
-   }
+    for (suit in suits) {
+        for (rank in ranks) {
+            cards.add(rank + suit)
+        }
+    }
+    cards.shuffle()
+    return cards
 }
 
-fun get() {
-    TODO("Not yet implemented")
+fun get(cards: MutableList<String>) {
+    var success = false
+
+    while (!success){
+        println("Number of cards:")
+        val numberOfCards: Int
+
+        try {
+            numberOfCards = readln().toInt()
+        } catch (e: NumberFormatException) {
+            println("Invalid number of cards.")
+            break
+        }
+
+        if (numberOfCards <= 0 || numberOfCards > 52) {
+            println("Invalid number of cards.")
+            break
+        }
+
+        if (cards.size < numberOfCards) {
+            println("The remaining cards are insufficient to meet the request.")
+            break
+        }
+
+        for (index in cards.lastIndex downTo cards.lastIndex - numberOfCards + 1) {
+            print(cards[index] + " ")
+            cards.removeAt(index)
+        }
+
+        println()
+        success = true
+    }
+
 }
 
-fun shuffleDeck() {
-    TODO("Not yet implemented")
+fun shuffleDeck(cards: MutableList<String>) {
+    cards.shuffle()
+    println("Card deck is shuffled.")
 }
 
-fun resetDeck() {
-    TODO("Not yet implemented")
+fun resetDeck(): MutableList<String> {
+    println("Card deck is reset.")
+    return createDeck()
 }
