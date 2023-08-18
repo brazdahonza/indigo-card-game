c5280060bc423dd326095c0aab5719938451ca6a
Jan Brazda <brazdahonza@gmail.com>
2023-08-18 14:32:01 +0200

    function startGame added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,36 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    startGame()
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun startGame() {
+    val ranks = listOf("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
+    val suits = listOf("♦", "♥", "♠", "♣")
+    val cards = mutableListOf<String>()
+    var gameRunning: Boolean = true
+
+   while(gameRunning) {
+       println("Choose an action (reset, shuffle, get, exit):")
+       val option = readln()
+       when(option) {
+           "reset" -> resetDeck()
+           "shuffle" -> shuffleDeck()
+           "get" -> get()
+           "exit" -> {
+               gameRunning = false
+               println("Bye")
+           }
+       }
+   }
+}
+
+fun get() {
+    TODO("Not yet implemented")
+}
+
+fun shuffleDeck() {
+    TODO("Not yet implemented")
+}
+
+fun resetDeck() {
+    TODO("Not yet implemented")
 }
